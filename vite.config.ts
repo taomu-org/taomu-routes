e8a2a3d355074d291fd1eedb3585129778673a1f
lanten <lanten233@qq.com>
2024-08-19 18:15:55 +0800

    chore: 依赖项优化

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 
   build: {
     rollupOptions: {
-      external: ['react', 'react-router-dom'],
+      external: ['react', 'react-router-dom', 'taomu-toolkit'],
     },
     lib: {
       name: 'taomu-routes',
