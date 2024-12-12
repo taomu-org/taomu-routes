ee6487389abc58ec09585f0e19334e3b5e5bcbae
lanten <lanten233@qq.com>
2024-12-12 18:06:12 +0800

    fix: rm out dir

--- out/components/index.js
+++
@@ -1,4 +0,0 @@
-export * from './router.ctx';
-export * from './async-import';
-export * from './app-router';
-export * from './link';
--- out/routes/index.js
+++
@@ -1,2 +0,0 @@
-export * from './auto-routes';
-export * as routeTools from './route-tools';
